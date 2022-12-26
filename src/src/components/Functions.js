00b28b16f3096755b72ae9312ef43d7937cd9aca
Htoo Aung Lin <59634121+Htoo23@users.noreply.github.com>
2022-12-26 15:15:36 +0630

    Add files via upload

---
+++ src/src/components/Functions.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class Functions extends Component{
+    render(){
+        return(
+            <div className="each-function" onClick={()=>this.props.functionClick(this.props.func)}>
+                <h2 className="special-button-name">{this.props.func}</h2>
+
+            </div>
+        );
+    }
+}
+export default Functions;
